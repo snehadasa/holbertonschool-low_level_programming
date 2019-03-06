d109c43a748f57c406cb0c07b6a018bfc0e4e181
snehadasa <sneha.dasasneha@gmail.com>
2019-03-05 22:30:05 -0800

    2d arrays

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -8,7 +8,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, j;
 	int **array;
 
 	if (width <= 0 || height <= 0)
@@ -29,8 +29,19 @@ int **alloc_grid(int width, int height)
 
 		if (!array[i])
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(array[j]);
+			}
+			free(array);
+
 			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
 	}
+
 	return (array);
 }
