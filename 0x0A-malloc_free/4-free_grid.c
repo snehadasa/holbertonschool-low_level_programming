9c63fa5543fcc1532bde5f305f7cb765ae3ad6d7
snehadasa <sneha.dasasneha@gmail.com>
2019-03-05 22:41:37 -0800

    free

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2 dimensional grid previously created in alloc_grrid.
+ * @grid: previous grid
+ * @height: height in grid.
+ * Return: NULL if size = 0, pointer to the copied string  or NULL if it fails.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (!grid || sizeof(grid) == 0)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
