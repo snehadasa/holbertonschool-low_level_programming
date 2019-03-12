5f38a5a1cd97fd6f21d53f4b2c36ddce7328fcb4
snehadasa <sneha.dasasneha@gmail.com>
2019-03-11 18:10:21 -0700

    struct and headers

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,11 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - its a structure by name dog.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: owner of the dog.
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct in a big kitty world
  */
 struct dog
 {
@@ -13,9 +15,9 @@ struct dog
 	float age;
 	char *owner;
 };
-dog_t;
 
-int _putchar(char c);
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
