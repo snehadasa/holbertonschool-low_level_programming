52da4b945f5ee2a38a92a817a1ba6bb2e4942889
snehadasa <sneha.dasasneha@gmail.com>
2019-03-16 12:26:10 -0700

    Fixing 3 question edge case

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -3,11 +3,9 @@
 /**
  * print_all - prints all the arguments which are in main.
  * ('H', 'O', 'lberton')
- *
  * @format: list of types of arguments passed to the func.
  * (c: char, i: integer, f: float, s: char *)
  * if string is null, print (nil).
- *
  * Return: void(return nothing).
  */
 void print_all(const char * const format, ...)
@@ -15,38 +13,36 @@ void print_all(const char * const format, ...)
 	va_list list;
 	char *sep = "";
 	int i;
+	char *str;
 
 	va_start(list, format);
-
 	i = 0;
-
 	while (format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
 				printf("%s%c", sep, va_arg(list, int));
-				/*the sep is printing blank("")for the first letter*/
+				/*sep is printing ("")for the first letter*/
 				break;
-
 			case 'i':
 				printf("%s%d", sep, va_arg(list, int));
 				break;
-
 			case 'f':
 				printf("%s%f", sep, va_arg(list, double));
 				break;
-
 			case 's':
-				printf("%s%s", sep, va_arg(list, char *));
+				str = va_arg(list, char *);
+				if (!str)
+					printf("%snil", sep);
+				else
+					printf("%s%s", sep, str);
 				break;
-
 			default:
 				i++; /*incrementing to the next character.*/
 				continue; /*going back to the while loop*/
 		}
-		sep = ", ";
-		/*the sep is printing , and space( )for the remaining letters*/
+		sep = ", ";/*the sep is printing , and space( )for the remaining letters*/
 		i++;
 	}
 	va_end(list);
