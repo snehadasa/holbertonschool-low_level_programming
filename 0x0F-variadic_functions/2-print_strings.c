f9920e7f5b24954815196ee17cb631b84bf21a68
snehadasa <sneha.dasasneha@gmail.com>
2019-03-16 12:32:40 -0700

    typo

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -27,11 +27,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 
-		if (i != n-1 && separator)
+		if (i != n - 1 && separator)
 		{
 			printf("%s", separator);
 		}
-		
 	}
 	printf("\n");
 	va_end(list);
