9fc7974a30f5fbf47cd27e691977d1b3d9ed61b9
snehadasa <sneha.dasasneha@gmail.com>
2019-03-15 17:44:40 -0700

    print numbers

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers which are in the main
+ * that is addressed here by the constant variable n.
+ *
+ * @separator: it is a string to be printed between numbers.
+ *
+ * @n: number of integers passed to the function.
+ *
+ * Return: void(return nothing).
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator)
+		{
+			printf("%d ,", va_arg(list, int));
+		}
+		else if (separator)
+		{
+			printf("%d ,", va_arg(list, int));
+		}
+		else
+		{
+			printf("%s%d ,", separator, va_arg(list, int));
+		}
+
+	}
+
+	va_end(list);
+
+	printf("\n");
+}
