b8e75d1132729530f6065f05a2aed489163256e0
snehadasa <sneha.dasasneha@gmail.com>
2019-03-21 13:35:14 -0700

    editted description

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -3,10 +3,9 @@
 #include "lists.h"
 
 /**
- * list_len - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed(i)
+ * list_len - gives the length of the list
+ * @h: pointer to the list_t list to get the length.
+ * Return: the number of elements in the linked list(i).
  */
 size_t list_len(const list_t *h)
 {
