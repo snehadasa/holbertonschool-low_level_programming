e9b9c14f503e4be0e828df9048b51abaff2c95c1
snehadasa <sneha.dasasneha@gmail.com>
2019-03-26 10:59:24 -0700

    initialising the temp1 variable to head

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,12 +32,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (-1);
 		temp1 = temp1->next; /*temp1 points to (n-1) node*/
 	}
+	temp2 = *head;
+
+	if (temp2->next == NULL)
+		return (-1);
 
 	temp2 = temp1->next; /*nth node*/
-	if (temp2->next)
-	{
-		temp1->next = temp2->next; /*(n+1)th node*/
-	}
+	temp1->next = temp2->next; /*(n+1)th node*/
 	free(temp2); /*delete temp2*/
 
 	return (1);
