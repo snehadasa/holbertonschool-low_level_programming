da62106d0601a47209ed3b1f1c4dfb3a8394de56
snehadasa <sneha.dasasneha@gmail.com>
2019-03-28 16:37:09 -0700

    fixed 1

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -7,13 +7,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bit = sizeof(n) * 8; /*size of unsigned long = 8*/
+	int bit; /*sizeof(n) * 8;size of unsigned long = 8, bit = 64*/
 	int count = 0;
+	unsigned long int num;
 
-	while (bit)
+	for (bit = 63; bit >= 0; bit--)
 	{
-		if (n >> bit & 1) /*by doing &1 we are checking for last digit
-			       if its 1 or 0*/
+		num = n >> bit;
+
+		if (num& 1) /*&1 is checking for last digit if its 1 or 0*/
 		{
 			_putchar('1');
 			count++;
