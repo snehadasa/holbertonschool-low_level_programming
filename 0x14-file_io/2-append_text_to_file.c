15bb4a31a379ef197a624f5515b66b0abe449888
snehadasa <sneha.dasasneha@gmail.com>
2019-04-02 21:43:01 -0700

    infinite loop

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content)
+		while (text_content[re])
 			re++;
 		if (re > 0)
 			wr = write(fd, text_content, re);
