c0e6dd1e1372427c62660adbc42b5da9cb384fe9
snehadasa <sneha.dasasneha@gmail.com>
2019-04-02 19:56:56 -0700

    append the file

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - creates a file and fills it with text
+ * @filename: name of the file to append
+ * @text_content: null terminating string to add at the end of the file.
+ * Return: 1 if file exists, -1 if the file doesn't.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, re = 0, wr;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+	while (text_content)
+		re++;
+	wr = write(fd, text_content, re);
+	if (wr != re)
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
