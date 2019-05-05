85c532910addd041a8907cab480f9ca8a4e58f8b
snehadasa <sneha.dasasneha@gmail.com>
2019-05-05 07:02:54 -0700

    insert at index 0 fix

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = *h;
 		new->prev = NULL;
 		*h = new;
+		if (new->next)
+			new->next->prev = new;
+
 		return (new);
 	}
 
