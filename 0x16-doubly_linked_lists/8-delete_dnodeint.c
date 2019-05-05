c3eec344937db9ef69761f6cf2c16852473ced3c
snehadasa <sneha.dasasneha@gmail.com>
2019-05-05 07:46:34 -0700

    eedge case fix

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,6 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (*head)
 			(*head)->prev = NULL;
 		free(delete_node);
+		return (1);
 	}
 
 	for (i = 0; i < index - 1; i++)
