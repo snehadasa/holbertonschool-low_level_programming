6ed0025afff557d9f6e265a56cf0ece58bdfdc9b
snehadasa <sneha.dasasneha@gmail.com>
2019-05-04 17:51:00 -0700

    prints elemnts of the list

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elements present in the list.
+ * @h: list to be printed
+ * Return: elements in the list "h".
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
