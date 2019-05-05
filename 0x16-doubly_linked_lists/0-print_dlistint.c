106c9c0a22b15022ed93642769aa4e5fa7481cb2
snehadasa <sneha.dasasneha@gmail.com>
2019-05-04 18:10:41 -0700

    if h is null

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,16 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
+
 	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
