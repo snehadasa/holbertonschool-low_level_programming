90d88b864fd3cfbd86ad44eefa34c9143b56a8a0
snehadasa <sneha.dasasneha@gmail.com>
2019-05-05 06:53:56 -0700

    Revert "insert node at given index"
    This reverts commit 8e4fe048c21766225881f64b3cdb5e86ffcf3413.

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (temp->next == NULL)
-			break;
+			return (NULL);
 
 		temp = temp->next;
 	}
