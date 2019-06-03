d0893012d2ad636645f6449fe5d4dc5bb5caaa44
snehadasa <sneha.dasasneha@gmail.com>
2019-06-02 21:48:17 -0700

    add node

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: hash table.
+ * @key: key.
+ * @value: value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *new;
+	hash_node_t *head;
+
+	if (ht->size == 0)
+		return (0);
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	i = key_index((unsigned char *)key, ht->size);
+
+	new->value = strdup(value);
+
+	head = ht->array[i];
+	new->next = head;
+	ht->array[i] = new;
+	return (1);
+}
