68a6ae0fc19a8e28304684dcb995d2aaf10d94a4
snehadasa <sneha.dasasneha@gmail.com>
2019-06-02 21:51:28 -0700

    strdup key

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -24,6 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	i = key_index((unsigned char *)key, ht->size);
 
 	new->value = strdup(value);
+	new->key = strdup(key);
 
 	head = ht->array[i];
 	new->next = head;
