552543c8d85bcf2c1ff0b2c97d03d9b7e0cab1a9
snehadasa <sneha.dasasneha@gmail.com>
2019-06-03 22:30:13 -0700

    braces

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int j;
 	hash_node_t *temp;
+	int flag = 0;
 
 	if (!ht)
 		return;
@@ -19,9 +20,10 @@ void hash_table_print(const hash_table_t *ht)
 		temp = ht->array[j];
 		while (temp)
 		{
-			printf("'%s': '%s'", temp->key, temp->value);
-			if (j != ht->size - 1 && temp->next != NULL)
+			if (flag)
 				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
 			temp = temp->next;
 		}
 	}
