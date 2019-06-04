e999e57c5442c1972318c47da6d903de7b1ca4c1
snehadasa <sneha.dasasneha@gmail.com>
2019-06-03 21:46:55 -0700

    null check

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -64,7 +64,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	hash_node_t *head;
 
-	if (!key || !value)
+	if (!ht || !key || !value)
 		return (0);
 
 	if (ht->size == 0)
