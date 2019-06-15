f332502f87ca5373f45109f3f12afb968a5cbaa0
snehadasa <sneha.dasasneha@gmail.com>
2019-06-15 11:45:07 -0700

    quick sort

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,8 +1,62 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps two numbers
+ * @n1: First number
+ * @n2: Second number
+ * Return: Nothing
+ */
+void swap_node(listint_t **head)
+{
+	listint_t *prev;
+
+	prev = (*head)->prev;
+	if ((*head)->prev->prev)
+		(*head)->prev->prev->next = *head;
+	(*head)->prev = (*head)->prev->prev;
+	prev->prev = *head;
+	prev->next = (*head)->next;
+	(*head)->next = prev;
+	(*head)->next = prev;
+}
+
+/**
+ * insertion_sort_list - Insertion sort
+ * @list: Linked list to be sorted
+ * Return: Nothing
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *head  = NULL;
+	/* listint_t *prev = NULL; */
+	/* listint_t *tmp = NULL; */
+	listint_t *iter = NULL;
+	/* listint_t *newList = NULL; */
 
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	head = (*list);
+	while(head)
+	{
+		head = head->next;
+		if (head->n < head->prev->n)
+		{
+			swap_node(&head);
+			iter = head;
+			head = head->next;
+			while (iter)
+			{
+				if (iter->prev && iter->n < iter->prev->n)
+				{
+					swap_node(&iter);
+					iter = iter->prev;
+					print_list(*list);
+					return;
+				}
+				else
+					break;
+			}
+		}
+	}
 }
--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -3,38 +3,45 @@
 /**
  * partition_lomuto_sort - quick sort
  */
-int partition_lomuto_sort(int *array, int low, int high)
+void partition_lomuto_sort(int *array, int low, int high, int size)
 {
 	int current_low = low;
 	int current_high = high;
-	int pivot = array[high];
+	int pivot;
 
+	if (low >= high)
+		return;
+
+	pivot = array[high];
 	while (current_low < current_high)
 	{
 		while (current_low < current_high && array[current_low] < pivot)
 		{
 			current_low++;
-			printf("current_low: %d\n", current_low);
 		}
 		while (current_low < current_high && array[current_high] >= pivot)
 		{
 			current_high--;
-			printf("current_high: %d\n", current_high);
 		}
-		swap(&array[current_low], &array[current_high]);
+		if(current_low < current_high)
+		{
+			swap(&array[current_low], &array[current_high]);
+			print_array(array, size);
+		}
 	}
-	swap(&array[current_high], &array[high]);
-	return (current_high);
+	if (current_high < high)
+	{
+		swap(&array[current_high], &array[high]);
+		print_array(array, size);
+	}
+
+	partition_lomuto_sort(array, low, current_high - 1, size);
+	partition_lomuto_sort(array, current_high + 1, high, size);
 }
 
 /**
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t p;
-
-	p = partition_lomuto_sort(array, 0, size - 1);
-
-	quick_sort(array, p - 1);
-	quick_sort(array, p + 1);
+	partition_lomuto_sort(array, 0, size - 1, size);
 }
