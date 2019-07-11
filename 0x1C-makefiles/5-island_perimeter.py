f86887cbe64a95fb2e1ac94ad80a0a0a044b1e90
snehadasa <sneha.dasasneha@gmail.com>
2019-07-11 15:02:08 -0700

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Island perimeter that returns perimeter of the island"""
+
+def island_perimeter(grid):
---
+++ 0x1C-makefiles/5-main.py
@@ -0,0 +1,17 @@
+guillaume@ubuntu:~/0x1C$ cat 5-main.py
+#!/usr/bin/python3
+"""
+5-main
+"""
+island_perimeter = __import__('5-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
+
