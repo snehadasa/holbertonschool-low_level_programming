b88138382375edf6e5d8c9f66a13e1728cc1c173
snehadasa <sneha.dasasneha@gmail.com>
2019-07-22 14:03:55 -0700

    repo fix

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -29,6 +30,8 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
