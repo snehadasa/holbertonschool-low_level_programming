4293586a321a6e67665c8324cd294475c8b04376
snehadasa <sneha.dasasneha@gmail.com>
2019-07-22 14:08:28 -0700

    creating binary tree

---
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates new binary tree.
+ * parent: parent of the nodes of a binary tree.
+ * value: value associated with the node.
+ * Return: newly created binary tree.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+
+	return (new);
+}
