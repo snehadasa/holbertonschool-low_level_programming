d8e91cce10ca50c1d0d850f73ccc5c80fb12086c
snehadasa <sneha.dasasneha@gmail.com>
2019-07-22 23:34:10 -0700

    fix 9

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -1,22 +1,35 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height_rec - to check height of a binary tree.
+ * @tree: binary tree
+ * Return: height of the tree
+ */
+size_t binary_tree_height_rec(const binary_tree_t *tree)
+{
+	size_t hl, hr;
+
+	if (!tree)
+		return (0);
+
+	hl = binary_tree_height_rec(tree->left);
+	hr = binary_tree_height_rec(tree->right);
+
+	if (hl > hr)
+		return (hl + 1);
+	else
+		return (hr + 1);
+}
+
+/**
  * binary_tree_height - to check height of a binary tree.
  * @tree: binary tree
  * Return: height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int hl, hr;
-
 	if (!tree)
 		return (0);
 
-	hl = binary_tree_height(tree->left);
-	hr = binary_tree_height(tree->right);
-
-	if (hl > hr)
-		return (hl + 1);
-	else
-		return (hr + 1);
+	return (binary_tree_height_rec(tree) - 1);
 }
