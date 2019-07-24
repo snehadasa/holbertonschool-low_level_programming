52159f4e62ebe77538ce3258ebc1cc201af1f503
snehadasa <sneha.dasasneha@gmail.com>
2019-07-24 15:18:50 -0700

    level order

--- 0x1D-binary_trees/101-binary_tree_levelorder.c
+++ 0x1D-binary_trees/101-binary_tree_levelorder.c
@@ -1,22 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * printlevelorder - goes through binary tree using levelorder traversal
+ * print_levelorder - goes through binary tree using levelorder traversal
  * @tree: binary tree
  * @func: s a pointer to a function to call for each node
+ * @level: level of the tree
  * Return: void
  */
-int print_levelorder(const binary_tree_t *tree, int level, void (*func)(int))
+int print_levelorder(binary_tree_t *tree, int level, void (*func)(int))
 {
+	int l, r;
 	if (!tree)
-		return(0);
+		return (0);
 	if (level == 1)
 	{
 		func(tree->n);
 		return (1);
 	}
-	return ((print_levelorder(tree->left, level - 1, func)) ||
-			(print_levelorder(tree->right, level - 1, func)));
+	l = print_levelorder(tree->left, level - 1, func);
+	r = print_levelorder(tree->right, level - 1, func);
+
+	return (l || r);
 }
 
 /**
@@ -31,9 +35,8 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (!tree || !func)
 		return;
-	while (print_levelorder(tree, level, func))
+	while (print_levelorder((binary_tree_t *)tree, level, func))
 	{
-		printf("level %d", level);
 		level++;
 	}
 }
