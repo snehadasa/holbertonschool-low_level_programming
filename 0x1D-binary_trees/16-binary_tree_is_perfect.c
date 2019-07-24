24837a3f9d0d56d4c21376e1bcb8213e30a3e60a
snehadasa <sneha.dasasneha@gmail.com>
2019-07-24 12:35:51 -0700

    if tree is null

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -67,6 +67,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+
 	return (binary_tree_is_full(tree) == 1 &&
 			binary_tree_balance(tree) == 0);
 }
