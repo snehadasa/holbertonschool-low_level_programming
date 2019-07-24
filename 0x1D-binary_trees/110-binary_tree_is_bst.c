430f26d87a2087b5d1deb8f5891816db8911d463
snehadasa <sneha.dasasneha@gmail.com>
2019-07-24 11:11:55 -0700

    binar search tree

---
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - to check if a binary tree is a binary search tree.
+ * @tree: binary tree
+ * Return: 1 if yes
+ */
+int is_bst_util(const binary_tree_t *tree, int minvalue, int maxvalue)
+{
+	if (!tree)
+		return (1);
+
+	if (tree->n < minvalue || tree->n > maxvalue)
+		return (0);
+	if (is_bst_util(tree->left, minvalue, tree->n - 1) &&
+			is_bst_util(tree->right, tree->n + 1, maxvalue))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * binary_tree_is_bst - to check if a binary tree is a binary search tree.
+ * @tree: binary tree
+ * Return: 1 if yes
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (is_bst_util(tree, INT_MIN, INT_MAX));
+}
