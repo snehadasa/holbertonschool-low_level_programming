b05e2729533855f890d451b29f2c583f376ed7bf
snehadasa <sneha.dasasneha@gmail.com>
2019-07-25 12:40:20 -0700

    tasks fix

--- 0x1D-binary_trees/104-binary_tree_rotate_right.c
+++ 0x1D-binary_trees/104-binary_tree_rotate_right.c
@@ -14,8 +14,8 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	if (tree->left)
 	{
 		temp = tree->left;
-		tree->left = temp->right;
 		tree->parent = temp;
+		tree->left = temp->right;
 		temp->right = tree;
 	}
 	return (temp);
