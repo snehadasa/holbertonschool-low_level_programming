9bc3f0c1f8c4443b12efcc90bdefcaaabb73587f
snehadasa <sneha.dasasneha@gmail.com>
2019-07-25 10:33:54 -0700

    binary tree is perfect

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,69 +1,59 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - to check height of a binary tree.
+ * binary_tree_depth - to check depth of a binary tree.
  * @tree: binary tree
- * Return: height of the tree
+ * Return: depth of the tree
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t hl, hr;
+	size_t d = 0;
 
 	if (!tree)
-		return (-1);
-
-	hl = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	hr = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	if (hl > hr)
-		return (hl);
-	else
-		return (hr);
+		return (0);
+	while (tree)
+	{
+		d++;
+		tree = tree->left;
+	}
+	return (d);
 }
 
 /**
- * binary_tree_balance - to measure the balance factor of a binary tree.
- * balance factor = height(left-child) - height(right-child).
- * @tree: binary tree
- * Return: balance factor of the tree
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int balance_factor = 0;
-
-	balance_factor = binary_tree_height(tree->left) -
-		binary_tree_height(tree->right);
-	return (balance_factor);
-}
-
-/**
- * binary_tree_is_full - checks if binary tree is full
+ * binary_tree_height_rec - helper func to check if binary tree is perfect
  * @tree: binary tree
  * Return: 1 if yes
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_perfect_rec(const binary_tree_t *tree, int d, int level)
 {
 	if (!tree)
 		return (0);
-
 	if (!tree->left && !tree->right)
-		return (1);
+		return (d == level + 1);
 
-	if (tree->left && tree->right)
-	{
-		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
-	}
+	if (!tree->left || !tree->right)
+		return (0);
+
+	if (binary_tree_is_perfect_rec(tree->left, d, level + 1) &&
+			binary_tree_is_perfect_rec(tree->right, d, level + 1))
+		return (1);
 	return (0);
 }
 
 /**
- * binary_tree_is_perfect - checks if binary tree is perfect
+ * binary_tree_is_perfect - to check if a binary tree is perfect or not.
  * @tree: binary tree
  * Return: 1 if yes
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	return (binary_tree_is_full(tree) == 1 &&
-			binary_tree_balance(tree) == 0);
+	int d, p;
+
+	if (!tree)
+		return (0);
+
+	d = binary_tree_depth(tree);
+	p = binary_tree_is_perfect_rec(tree, d, 0);
+
+	return (p);
 }
